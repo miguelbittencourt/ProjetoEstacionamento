728649e7eb3c5620236e9a07ecfba58cecbf62e6
Vitor <Vitor@DESKTOP-10FAT2A>
2022-10-24 20:10:01 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,27 @@
+﻿using Estacionamento.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Estacionamento.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public static List<Usuario> lsUsuarios = new List<Usuario>();
+        public IActionResult Index()
+        {
+            return View(lsUsuarios);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario objeto)
+        {
+            lsUsuarios.Add(objeto);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace Estacionamento.Models
+{
+    public class Usuario
+    {
+        public int Codigo { get; set; }
+
+        public int Login { get; set; }
+
+        public int Senha { get; set; }
+    }
+}
